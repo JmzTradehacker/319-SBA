f8726c3bf2ca8b1515ea31926f78cfb2c837dd5b
JmzTradehacker <japhet.mdn@gmail.com>
2024-11-07 11:48:11 -0800

    Configured server and MongoDB connection in server.js.

---
+++ server.js
@@ -0,0 +1,22 @@
+import 'dotenv/config';
+import express from 'express';
+import mongoose from 'mongoose';
+
+const app = express();
+
+//Middleware
+app.use();
+app.use(express.json());
+
+//Mongoose - MongoDb Connection
+mongoose.connect(process.env.ATLAS_URI, {})
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('Error connecting to MongoDB:', err));
+
+//Routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/trades', require('./routes/trades'));
+
+//Server Runs/Listens
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server Running on port ${PORT}`))
